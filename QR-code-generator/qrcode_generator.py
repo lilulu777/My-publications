12551ea62e90375bc5027c6eb047b7a6c24b719e
Wenhhao <67314119+Wenhhao@users.noreply.github.com>
2024-10-15 16:05:53 +0800

    Create qrcode_generator.py

---
+++ QR-code-generator/qrcode_generator.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 15 13:17:33 2024
+
+@author: Wenhao, Lu
+"""
+
+
+import segno
+
+url = 'https://github.com/lilulu777/My-publications/project-MFT/author-list.md'
+qr = segno.make(url, error='h')
+qr.save('authors.png', scale=10, border = 1, dark='orange', light='#ffffff')
